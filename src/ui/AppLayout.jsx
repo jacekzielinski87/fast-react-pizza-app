842571014c8d8687d0de844167847880b56cba98
Jacek Zielinski <jacekzielinski0787@gmail.com>
2024-03-10 17:27:30 +0100

    Grid properties

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,25 +1,26 @@
-import Header from "./Header";
-import CartOverview from "../features/cart/CartOverview";
-import { Outlet, useNavigation } from "react-router-dom";
-import Loader from "./Loader";
+import Header from './Header';
+import CartOverview from '../features/cart/CartOverview';
+import { Outlet, useNavigation } from 'react-router-dom';
+import Loader from './Loader';
 
 function AppLayout() {
-    const navigation = useNavigation();
-    const isLoading = navigation.state === 'loading';
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
 
-    return (
-        <div className="layout">
-            {isLoading && <Loader/>}
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
 
-            <Header/>
+      <Header />
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
 
-            <main>
-                <Outlet/>
-            </main>
-
-            <CartOverview/>
-        </div>
-    )
+      <CartOverview />
+    </div>
+  );
 }
 
-export default AppLayout
+export default AppLayout;
